9306cd0a94aae329e2eefff82978866c0d5684a8
David Kleiven <davidkleiven446@gmail.com>
2019-06-30 13:23:07 +0200

    Add a few utility functions

--- src/main/java/com/github/jray/TriangleMesh.java
+++ src/main/java/com/github/jray/TriangleMesh.java
@@ -18,8 +18,32 @@ public class TriangleMesh
         nodes.add(p);
     }
 
+    public void addElement(int[] ids)
+    {
+        nodeIds.add(ids);
+    }
+
+    public int numElements()
+    {
+        return nodeIds.size();
+    }
+
+    /**
+     * Return the number of nodes
+     * @return
+     */
     public int numNodes()
     {
         return nodes.size();
+    }
+
+    public Point normal(int element)
+    {
+        int[] ids = nodeIds.get(element);
+        Point p1 = nodes.get(ids[1]).distance(nodes.get(ids[0]));
+        Point p2 = nodes.get(ids[2]).distance(nodes.get(ids[0]));
+        Point p3 = p1.cross(p2);
+        p3.divide(p3.length());
+        return p3;
     }
 }

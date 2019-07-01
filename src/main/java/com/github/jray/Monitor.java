15e7c0427d1424badcf483874b5f3f1c0f545336
David Kleiven <davidkleiven446@gmail.com>
2019-07-01 20:55:56 +0200

    Add read write to simple CSV format. Closes #14.

--- src/main/java/com/github/jray/Monitor.java
+++ src/main/java/com/github/jray/Monitor.java
@@ -1,6 +1,11 @@
 package com.github.jray;
 
 import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.BufferedReader;
 
 public class Monitor
 {
@@ -15,6 +20,16 @@ public class Monitor
         }
     }
 
+    public ArrayList<Vector> getReal()
+    {
+        return new ArrayList<Vector>(amplitudeReal);
+    }
+
+    public ArrayList<Vector> getImag()
+    {
+        return new ArrayList<Vector>(amplitudeImag);
+    }
+
     public void update(int element, Ray ray)
     {
         double opl = ray.getWavenumber()*ray.opticalPathLength;
@@ -22,5 +37,52 @@ public class Monitor
         Vector imagPart = ray.getAmplitude().mult(Math.sin(opl));
         amplitudeReal.get(element).iadd(realPart);
         amplitudeImag.get(element).iadd(imagPart);
+    }
+
+    public void save(String fname)
+    {
+        try{
+            FileWriter writer = new FileWriter(fname);
+            writer.write("# RealX,RealY,RealZ,ImagX,ImagY,ImagZ\n");
+            for (int i=0;i<amplitudeReal.size();i++)
+            {
+                Vector re = amplitudeReal.get(i);
+                Vector im = amplitudeImag.get(i);
+				writer.write(String.format("%f,%f,%f,%f,%f,%f\n", re.getX(), re.getY(),re.getZ(), im.getX(), im.getY(), im.getZ()));
+            }
+            writer.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void load(String fname)
+    {
+        amplitudeReal.clear();
+        amplitudeImag.clear();
+
+        try(BufferedReader br = Files.newBufferedReader(Paths.get(fname))){
+            String line;
+
+            while ((line = br.readLine()) != null){
+                if (line.startsWith("#")){
+                    continue;
+                }
+
+                String[] partitioned = line.split(",");
+                double reX = Double.parseDouble(partitioned[0]);
+                double reY = Double.parseDouble(partitioned[1]);
+                double reZ = Double.parseDouble(partitioned[2]);
+                double imX = Double.parseDouble(partitioned[3]);
+                double imY = Double.parseDouble(partitioned[4]);
+                double imZ = Double.parseDouble(partitioned[5]);
+
+                amplitudeReal.add(new Vector(reX, reY, reZ));
+                amplitudeImag.add(new Vector(imX, imY, imZ));
+            }
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/test/java/com/github/jray/MonitorTest.java
@@ -0,0 +1,45 @@
+package com.github.jray;
+
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MonitorTest
+{
+    private static final double EPS = 1E-6;
+
+    @Test
+    public void saveLoad()
+    {
+        Monitor monitor = new Monitor();
+        monitor.setSize(10);
+
+        Ray ray = new Ray();
+
+        // Insert some data
+        for (int i=0;i<40;i++)
+        {
+            ray.opticalPathLength = 0.1*i;
+            monitor.update(i%10, ray);
+        }
+        monitor.save("testMonitor.csv");
+
+        ArrayList<Vector> real = monitor.getReal();
+        ArrayList<Vector> imag = monitor.getImag();
+
+        monitor.load("testMonitor.csv");
+
+        ArrayList<Vector> real2 = monitor.getReal();
+        ArrayList<Vector> imag2 = monitor.getImag();
+
+        for (int i=0;i<real.size();i++){
+            assertEquals(real.get(i).getX(), real2.get(i).getX(), EPS);
+            assertEquals(real.get(i).getY(), real2.get(i).getY(), EPS);
+            assertEquals(real.get(i).getZ(), real2.get(i).getZ(), EPS);
+            assertEquals(imag.get(i).getX(), imag2.get(i).getX(), EPS);
+            assertEquals(imag.get(i).getY(), imag2.get(i).getY(), EPS);
+            assertEquals(imag.get(i).getZ(), imag2.get(i).getZ(), EPS);
+        }
+    }
+}

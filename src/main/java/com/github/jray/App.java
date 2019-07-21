07d8f63aafda53e08d17775cc72b4da9dc8e9555
David Kleiven <davidkleiven446@gmail.com>
2019-07-21 12:50:22 +0200

    Window appears

--- src/main/java/com/github/jray/App.java
+++ src/main/java/com/github/jray/App.java
@@ -1,13 +1,26 @@
 package com.github.jray;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        
+        StackPane root = new StackPane();
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }

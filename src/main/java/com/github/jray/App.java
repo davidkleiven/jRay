643e7527dc44db2baf593e5660a9f2b640c48223
David Kleiven <davidkleiven446@gmail.com>
2019-07-22 19:36:49 +0200

    Minor changes

--- src/main/java/com/github/jray/App.java
+++ src/main/java/com/github/jray/App.java
@@ -5,6 +5,11 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+
+import java.util.List;
+
 
 /**
  * jRay Application
@@ -12,6 +17,8 @@ import javafx.fxml.FXMLLoader;
  */
 public class App extends Application
 {
+    private List<InputCard> cards;
+
     public static void main(String[] args)
     {
         launch(args);
@@ -24,5 +31,10 @@ public class App extends Application
         primaryStage.setTitle("jRay");
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
+    }
+
+    @FXML
+    private void addMeshGeometry(ActionEvent event){
+
     }
 }
